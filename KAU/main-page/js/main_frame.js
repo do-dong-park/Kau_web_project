eb5d5917f174030e80484fb042dca9e52f7f279a
ssemee <tpals555@daum.net>
2021-11-27 12:52:00 +0900

    test

--- KAU/main-page/js/main_frame.js
+++ KAU/main-page/js/main_frame.js
@@ -6,6 +6,7 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+//test
 // Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
@@ -16,8 +17,8 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     // 3번 사진에서 1번사진으로 넘겨줌. 0번 사진으로가면, 3번으로 넘어감.
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     //내가 보는 사진 아닌 사진들은, 안보여야 함.
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -28,6 +29,6 @@ function showSlides(n) {
     }
     //내 위치에 존재하는 사진들을 보여주고 점을 활성화 시킴
     // 배열이기 때문에 위치 -1 로 현 위치를 표현.
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
